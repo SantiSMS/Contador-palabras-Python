424b171fb7c7fb0c7309d060a1172e58319f09a6
Santi.S. <santi.slongo@gmail.com>
2024-11-21 13:43:20 -0300

    Contador de palabras en base a algoritmo, pseudocódigo y diagrama de flujo

---
+++ contador-letras.py
@@ -0,0 +1,11 @@
+# PASO 1 - SOLICITAR PALABRAS
+
+palabra_ingresada = input("Por favor ingresar palabra: ")
+
+# PASO 2 - CONTAR LETRAS
+
+cant_letras = len(palabra_ingresada)
+
+# PASO 3 - MOSTRAR RESULTADO
+
+print("La palabra ingresada tiene", cant_letras, "letras")
